ae4198032bf6ad0398bbe343129b36d90be663e0
avnersch <avnersch@users.noreply.github.com>
2018-01-07 23:11:38 +0200

    upload files

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 import animate from "./animateplus.js";
 
-const path = document.querySelector("path");
-const length = path.getTotalLength();
-
-Object.assign(path.style, {
-  "stroke": "#616AFF",
-  "stroke-width": 10,
-  "stroke-dasharray": length
-});
-
-animate({
-  elements: path,
-  easing: "in-out-quartic",
-  duration: 5000,
-  loop: true,
-  direction: "alternate",
-  "stroke-dashoffset": [length, 0]
-});
+const paths = document.querySelectorAll("path");
+paths.forEach(path => {
+  const length = path.getTotalLength();
+  Object.assign(path.style, {
+    "stroke": "#3D90D5",
+    "stroke-width": 10,
+    "stroke-dasharray": length
+  });
+  animate({
+    elements: path,
+    easing: "in-out-quartic",
+    duration: 5000,
+    loop: true,
+    direction: "alternate",
+    "stroke-dashoffset": [length, 0]
+  });
+})
